550f2bd67144f65ab8aefc46d83527c3e10959d2
RedZ <155829210+NayonOP@users.noreply.github.com>
2025-05-31 00:05:15 +0600

    Update RedZHub.lua

--- RedZHub.lua
+++ RedZHub.lua
@@ -1,12 +1,9 @@
-
 SpeedTween = 300
 getgenv().RandomFruit = true
 getgenv().EspFruit = true
 getgenv().Team = "Marines"
 getgenv().WebhookUrl = ''
 loadstring(game:HttpGet('https://raw.githubusercontent.com/VNT-UNIVERSAL/Panda-Hub/main/Release/fruitfarm.lua'))()
-
-
 
 
 
